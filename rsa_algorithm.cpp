e758fde1b8192d60ee418dc77b000b8b405b5f32
Atharva Mahesh Amrutkar <atharva.21910993@viit.ac.in>
2022-02-03 22:15:34 +0530

    Update rsa_algorithm.cpp

--- rsa_algorithm.cpp
+++ rsa_algorithm.cpp
@@ -1,62 +1,159 @@
 #include<bits/stdc++.h>
-#include<stdio.h>
+#include <iostream>
+#include <stdlib.h>
 using namespace std;
 
-double p,q,n,e,phi,d;
-int k;
+long int p, q, n, t, temp[100], j;
+long int decryptedText[100], encryptedText[100], i, len;
+bool flag;
+string msg;
 
-int gcd(int a, int b) {
-	if(b == 0)
-		return a;
-	return gcd(b , a%b);
+void enerypt(long int);
+void decrypt(long int);
+long int gcalculateD(long int a, long int b);
+bool isPrime(long int);
+long int calculateE();
+long int calculateD(long int);
 
+bool isPrime(long int pr)
+{
+    int i;
+    j = (long int)sqrt((long double)pr);
+    for (int i = 2; i <= j; i++)
+    {
+        if (pr % i == 0)
+            return false;
+    }
+    return true;
 }
 
-string Encrypt_message(string message){
-	string encrypted_message;
-  	for(char p : message){
-    // Encryption c = (msg ^ e) % n 
-    char c = pow(p, e); 
-    c = fmod(c, n); 
-    encrypted_message += c;; 
-	}
-    return encrypted_message;
+long int calculateE()
+{
+    for (i = 2; i < t; i++)
+    {
+        if (gcalculateD(i, t) == 1)
+            return i;
+    }
+    return -1;
 }
-string Decrypt_message(string message){
-	string decrypted_message;
-	for(auto c : message){
-    // Decryption m = (c ^ d) % n 
-    double m = pow(c, d); 
-    m = fmod(m, n); 
-    decrypted_message += m;
-	}
-	return decrypted_message;
 
+long int calculateD(long int x)
+{
+    long long k = 1;
+    while (1)
+    {
+        k = k + t;
+        if (k % x == 0)
+            return (k / x);
+    }
 }
-void create_pair(){
-	p = 7; 
-    q = 11; 
-  
-    n = p*q; 
-  
-    e = 2; 
-    phi = (p-1)*(q-1); 
-    while (e < phi) { 
-        if (gcd(e, phi)==1) break; 
-        else e++; 
-    } 
-  
-    // choosing d such that it satisfies d*e = 1 + k * totient 
-    k = 2;  // A constant value 
-    d = (1 + (k*phi))/e; 
-  
+
+long int gcalculateD(long int a, long int b)
+{
+    return (b == 0) ? a : gcalculateD(b,a%b);
 }
-int main() { 
-	create_pair();
-    string message;
-	cin>>message;
-	cout<<Encrypt_message(message)<<endl<<Decrypt_message(Encrypt_message(message));
-	
-	
-    return 0; 
+
+void encrypt(long long e)
+{
+    long long pt, ct, key = e, k;
+    i = 0;
+    len = msg.size();
+    while (i != len)
+    {
+        pt = decryptedText[i];
+        pt = pt - 97;
+        k = 1;
+        for (j = 0; j < key; j++)
+        {
+            k = k * pt;
+            k = k % n;
+        }
+        temp[i] = k;
+        ct = k + 97;
+        encryptedText[i] = ct;
+        i++;
+    }
+    encryptedText[i] = -1;
+    cout << "\nTHE ENCRYPTED MESSAGE IS \n";
+    for (i = 0; i < len; i++)
+    {
+        printf("%c", encryptedText[i]);
+    }
+}
+
+void decrypt(long int d)
+{
+    long int pt, ct, key = d, k;
+    i = 0;
+    while (encryptedText[i] != -1)
+    {
+        ct = temp[i];
+        k = 1;
+        for (j = 0; j < key; j++)
+        {
+            k = k * ct;
+            k = k % n;
+        }
+        pt = k + 97;
+        decryptedText[i] = pt;
+        i++;
+    }
+    decryptedText[i] = -1;
+    cout << "\nTHE DECRYPTED MESSAGE IS \n";
+    for (i = 0; i < len; i++)
+    {
+        printf("%c", decryptedText[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+
+    cout << "Welcome To our RSA progrom" << endl;
+
+    do
+    {
+        cout << "Enter a Prime number P :" << endl;
+        cin >> p;
+        flag = isPrime(p);
+
+        if (flag == false)
+        {
+            cout << "\nWRONG INPUT\n";
+        }
+    } while (flag == 0);
+
+    do
+    {
+        cout << "Enter a Prime number Q :" << endl;
+        cin >> q;
+        flag = isPrime(q);
+
+        if (flag == false)
+        {
+            cout << "\nWRONG INPUT\n";
+        }
+    } while (flag == 0);
+
+    cout << "\nEnter message to be Encrypted : \n";
+    fflush(stdin);
+
+    getline(cin, msg);
+    for (int i = 0; msg[i] != '\0'; i++)
+    {
+        decryptedText[i] = msg[i];
+    }
+
+    n = p * q;
+    t = (p - 1) * (q - 1);
+    long int e = calculateE();
+    long int d = calculateD(e);
+
+    encrypt(e);
+    decrypt(d);
+
+    system("PAUSE");
+
+    return 0;
 }
